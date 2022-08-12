f8f2639f5c567a4a93e185150a1ad0e61d179412
Lawrence-Tang <tangzongsheng@gmail.com>
2022-08-12 13:15:45 +0800

    build-uboot.sh: add debian-buster

--- build-uboot.sh
+++ build-uboot.sh
@@ -61,7 +61,7 @@ fi
 true ${TARGET_OS:=${1,,}}
 
 case ${TARGET_OS} in
-buildroot* | friendlycore* | friendlywrt* | eflasher)
+buildroot* | friendlycore-focal-arm64 | debian-buster-* | friendlywrt* | eflasher )
         ;;
 *)
         echo "Error: Unsupported target OS: ${TARGET_OS}"
