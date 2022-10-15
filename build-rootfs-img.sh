d3b82580f1de6088f122b14cae102c37c0ab3528
rampageservices <vonneh@gmail.com>
2022-10-16 00:42:10 +0800

    replace tempfile command with mktemp which is present on both fedora and debian systems.

--- build-rootfs-img.sh
+++ build-rootfs-img.sh
@@ -45,7 +45,7 @@ if [ ${IMG_SIZE} -eq 0 ]; then
     ROOTFS_SIZE=`du -s -B 1 ${ROOTFS_DIR} | cut -f1`
     # +1024m + 10% rootfs size
     MAX_IMG_SIZE=$((${ROOTFS_SIZE} + 1024*1024*1024 + ${ROOTFS_SIZE}/5))
-    TMPFILE=`tempfile`
+    TMPFILE=`mktemp`
     ${MKFS} -s -l ${MAX_IMG_SIZE} -a root -L rootfs /dev/null ${ROOTFS_DIR} > ${TMPFILE}
     IMG_SIZE=`cat ${TMPFILE} | grep "Suggest size:" | cut -f2 -d ':' | awk '{gsub(/^\s+|\s+$/, "");print}'`
     rm -f ${TMPFILE}
