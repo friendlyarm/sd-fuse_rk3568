3835f38a81cf7ada65bcb7840797f96313549e7b
Lawrence-Tang <tangzongsheng@gmail.com>
2024-09-02 23:13:35 +0800

    add sample script to demonstrate creating btrfs filesystem image

--- test/test-btrfs-rootfs.sh
+++ test/test-btrfs-rootfs.sh
@@ -2,10 +2,14 @@
 set -eu
 
 HTTP_SERVER=112.124.9.243
-SOC=rk3568
+KERNEL_URL=https://github.com/friendlyarm/kernel-rockchip
+KERNEL_BRANCH=nanopi6-v6.1.y
+KCFG=nanopi5_linux_defconfig
 
 # hack for me
 [ -f /etc/friendlyarm ] && source /etc/friendlyarm $(basename $(builtin cd ..; pwd))
+
+SOC=rk3568
 
 # clean
 mkdir -p tmp
@@ -14,14 +18,24 @@ sudo rm -rf tmp/*
 cd tmp
 git clone ../../.git sd-fuse
 cd sd-fuse
-wget --no-proxy http://${HTTP_SERVER}/dvdfiles/${SOC^^}/images-for-eflasher/ubuntu-focal-desktop-arm64-images.tgz
-tar xzf ubuntu-focal-desktop-arm64-images.tgz
+wget --no-proxy http://${HTTP_SERVER}/dvdfiles/${SOC^^}/images-for-eflasher/debian-bookworm-core-arm64-images.tgz
+tar xzf debian-bookworm-core-arm64-images.tgz
 wget --no-proxy http://${HTTP_SERVER}/dvdfiles/${SOC^^}/images-for-eflasher/emmc-flasher-images.tgz
 tar xzf emmc-flasher-images.tgz
-wget --no-proxy http://${HTTP_SERVER}/dvdfiles/${SOC^^}/rootfs/rootfs-ubuntu-focal-desktop-arm64.tgz
+wget --no-proxy http://${HTTP_SERVER}/dvdfiles/${SOC^^}/rootfs/rootfs-debian-bookworm-core-arm64.tgz
 
-sudo tar xzfp rootfs-ubuntu-focal-desktop-arm64.tgz --numeric-owner --same-owner
-sudo -E FS_TYPE=btrfs ./build-rootfs-img.sh ubuntu-focal-desktop-arm64/rootfs ubuntu-focal-desktop-arm64
+# build kernel to add btrfs config 
+[ -d kernel ] || git clone ${KERNEL_URL} --depth 1 -b ${KERNEL_BRANCH} kernel
+echo "CONFIG_BTRFS_FS=y" >> kernel/arch/arm64/configs/${KCFG}
+BUILD_THIRD_PARTY_DRIVER=1 KERNEL_SRC=$PWD/kernel ./build-kernel.sh debian-bookworm-core-arm64
 
-./mk-sd-image.sh ubuntu-focal-desktop-arm64
-./mk-emmc-image.sh ubuntu-focal-desktop-arm64
+# update kernel modules to rootfs
+sudo ./tools/extract-rootfs-tar.sh rootfs-debian-bookworm-core-arm64.tgz
+sudo rm -rf debian-bookworm-core-arm64/rootfs/lib/modules/*
+sudo rsync -a out/output_${SOC}_kmodules/lib/modules/* debian-bookworm-core-arm64/rootfs/lib/modules/
+
+# create rootfs.img with btrfs
+sudo -E FS_TYPE=btrfs ./build-rootfs-img.sh debian-bookworm-core-arm64/rootfs debian-bookworm-core-arm64
+
+./mk-sd-image.sh debian-bookworm-core-arm64
+./mk-emmc-image.sh debian-bookworm-core-arm64 autostart=yes
